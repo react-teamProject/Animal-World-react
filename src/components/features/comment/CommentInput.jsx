8e2dc304b9ff6e991fb71c2ac1b41a25a6cc2fae
hobak12 <tjsgud2564@naver.com>
2022-12-25 15:35:33 +0900

    Refactor: getElementById에서 useRef로 변경 후 focus

--- src/components/features/comment/CommentInput.jsx
+++ src/components/features/comment/CommentInput.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
 import { __postComment } from "../../../redux/modules/commentSlice";
-
 const CommentInput = ({ param }) => {
   //useState를 하나만 쓰고 객체 분해 할당하여 이용함.
   const [comment, setComment] = useState({
@@ -12,6 +11,10 @@ const CommentInput = ({ param }) => {
     pw: "",
     content: "",
   });
+
+  const userInput = useRef();
+  const pwInput = useRef();
+  const contentInput = useRef();
 
   const dispatch = useDispatch();
 
@@ -25,17 +28,18 @@ const CommentInput = ({ param }) => {
     //객체 분해 할당해주고 사용
     const { content, user, pw } = comment;
 
+    //useRef 이용하여 리팩토링
     if (!user) {
       alert("닉네임을 입력하세요");
-      document.getElementById("user").focus();
+      userInput.current.focus();
       return;
     } else if (!pw) {
       alert("비밀번호를 입력하세요");
-      document.getElementById("pw").focus();
+      pwInput.current.focus();
       return;
     } else if (!content) {
       alert("댓글을 입력하세요");
-      document.getElementById("content").focus();
+      contentInput.current.focus();
       return;
     }
 
@@ -70,6 +74,7 @@ const CommentInput = ({ param }) => {
           setComment({ ...comment, [id]: value });
         }}
         placeholder="닉네임"
+        ref={userInput}
       />
       <input
         type="password"
@@ -80,6 +85,7 @@ const CommentInput = ({ param }) => {
           setComment({ ...comment, [id]: value });
         }}
         placeholder="비밀번호"
+        ref={pwInput}
       />
       <textarea
         id="content"
@@ -89,6 +95,7 @@ const CommentInput = ({ param }) => {
           setComment({ ...comment, [id]: value });
         }}
         placeholder="댓글을 입력해주세요"
+        ref={contentInput}
       />
       <button>댓글 등록하기</button>
     </form>
