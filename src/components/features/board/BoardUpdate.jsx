7d16bc308837641b01e018946398252c7cd8899c
seoyeon-jung <jungluchia1204@gmail.com>
2022-12-25 10:42:37 +0900

    Feat: 비밀 입력시 수정/삭제 기능 추가

--- src/components/features/board/BoardUpdate.jsx
+++ src/components/features/board/BoardUpdate.jsx
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { __editBoards } from "../../../redux/modules/boardSlice";
 
 const BoardInput = () => {
   const param = useParams().id;
   const dispatch = useDispatch();
-  const navigator = useNavigate();
+  const board = useSelector((state) => state.boardSlice.board);
 
   const [state, setState] = useState({
     id: param,
     title: "",
     content: "",
   });
+
+  // 비밀번호 일치 시 삭제
+  const a = board.filter((item) => item.id === param).map((item) => item)[0];
+  const b = a["pw"];
+  const [pwTest, setPWTest] = useState("");
+
+  const pwChangeHandler = (event) => {
+    setPWTest(event.target.value);
+  };
 
   // onsubmitHandler
   const onSubmitHandler = (e) => {
@@ -33,15 +42,23 @@ const BoardInput = () => {
       content: content,
     };
 
-    dispatch(__editBoards(newBoard));
-
-    alert("수정했습니다!");
-    // navigator("/");
-    setState({
-      id: param,
-      title: "",
-      content: "",
-    });
+    if (pwTest === b) {
+      dispatch(__editBoards(newBoard));
+      alert("수정했습니다!");
+      setState({
+        id: param,
+        title: "",
+        content: "",
+      });
+    } else {
+      alert("비밀번호가 일치하지 않습니다.");
+      setState({
+        id: param,
+        title: "",
+        content: "",
+      });
+      return;
+    }
   };
 
   return (
@@ -65,6 +82,12 @@ const BoardInput = () => {
             setState({ ...state, [id]: value });
           }}
           placeholder="내용을 입력해주세요"
+        />
+        <input
+          id="pwpw"
+          type="password"
+          placeholder="비밀번호 입력"
+          onChange={pwChangeHandler}
         />
         <button>수정하기</button>
       </form>
--- src/pages/BoardDetail.jsx
+++ src/pages/BoardDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import Layout from "../components/UI/Layout";
@@ -11,7 +11,16 @@ const Detail = () => {
   const board = useSelector((state) => state.boardSlice.board);
   const navigator = useNavigate();
   const param = useParams().id;
-  console.log(board);
+  //console.log(board);
+
+  // 비밀번호 일치 시 삭제
+  const a = board.filter((item) => item.id === param).map((item) => item)[0];
+  const b = a["pw"];
+  const [pwTest, setPWTest] = useState("");
+
+  const pwChangeHandler = (event) => {
+    setPWTest(event.target.value);
+  };
 
   useEffect(() => {
     dispatch(__getBoards());
@@ -19,8 +28,13 @@ const Detail = () => {
 
   // delete handler
   const onDeleteHandler = (id) => {
-    dispatch(__deleteBoards(id));
-    navigator("/");
+    if (pwTest === b) {
+      dispatch(__deleteBoards(id));
+      navigator("/");
+    } else {
+      alert("비밀번호가 일치하지 않습니다.");
+      return;
+    }
   };
 
   return (
@@ -32,7 +46,13 @@ const Detail = () => {
             <div key={item.id}>
               <div>{item.title}</div>
               <div>{item.content}</div>
+              <div>비밀번호 임시: {item.pw}</div>
               <button onClick={() => onDeleteHandler(item.id)}>삭제</button>
+              <input
+                type="password"
+                placeholder="비밀번호 입력"
+                onChange={pwChangeHandler}
+              />
             </div>
           );
         })}
