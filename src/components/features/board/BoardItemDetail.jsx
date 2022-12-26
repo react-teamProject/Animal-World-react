1aa20dc68bd46580eb044e3bef1b5f1ed986110a
seoyeon-jung <jungluchia1204@gmail.com>
2022-12-26 12:51:08 +0900

    Fix: board 삭제/수정 버튼 토글 기능으로 변경

---
+++ src/components/features/board/BoardItemDetail.jsx
@@ -0,0 +1,169 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  __deleteBoards,
+  __getBoards,
+  __editBoards,
+} from "../../../redux/modules/boardSlice";
+import { useNavigate } from "react-router-dom";
+
+const BoardItemDetail = () => {
+  const dispatch = useDispatch();
+  const board = useSelector((state) => state.boardSlice.board);
+  const navigator = useNavigate();
+  const param = useParams().id;
+
+  const [state, setState] = useState({
+    id: param,
+    title: "",
+    content: "",
+    pw: "",
+  });
+
+  // 비밀번호 일치 시 삭제
+  const a = board.filter((item) => item.id === param).map((item) => item)[0];
+  const password = a["pw"];
+  const [pwTest, setPWTest] = useState("");
+
+  const pwChangeHandler = (event) => {
+    setPWTest(event.target.value);
+  };
+
+  useEffect(() => {
+    dispatch(__getBoards());
+  }, [dispatch]);
+
+  // delete handler
+  const onDeleteHandler = (id) => {
+    if (pwTest === password) {
+      dispatch(__deleteBoards(id));
+      navigator("/");
+    } else {
+      alert("비밀번호가 일치하지 않습니다.");
+      setPWTest("");
+    }
+  };
+
+  // edit handler
+  const onEditHandler = (e) => {
+    // 새로고침 방지
+    e.preventDefault();
+
+    const { title, content } = state;
+
+    // 입력칸 공백 방지
+    if (!title || !content) {
+      alert("제목과 내용 모두 입력하세요");
+      return;
+    }
+
+    if (pwTest === password) {
+      const newBoard = {
+        id: param,
+        title: title,
+        content: content,
+      };
+      dispatch(__editBoards(newBoard));
+      alert("수정했습니다!");
+      toggleEditInput();
+    } else {
+      alert("비밀번호가 일치하지 않습니다.");
+      setPWTest("");
+    }
+  };
+
+  // show 여부를 결정하는 state
+  const [showEdit, setShowEdit] = useState(true);
+  const [showDelete, setShowDelete] = useState(true);
+
+  // 수정 input toggle
+  const toggleEditInput = () => {
+    setShowEdit((current) => !current);
+    setPWTest("");
+  };
+
+  // delete input toggle
+  const toggleDeleteInput = () => {
+    setShowDelete((current) => !current);
+    setPWTest("");
+  };
+
+  return (
+    <>
+      {board
+        .filter((item) => item.id === param)
+        .map((item) => {
+          return (
+            <div key={item.id}>
+              <div style={{ display: showEdit ? "block" : "none" }}>
+                {item.title}
+              </div>
+              <div style={{ display: showEdit ? "block" : "none" }}>
+                {item.content}
+              </div>
+
+              <div>비밀번호 임시: {item.pw}</div>
+
+              <div
+                style={{ display: showDelete && showEdit ? "block" : "none" }}
+              >
+                <button onClick={toggleDeleteInput}>삭제</button>
+                <button onClick={toggleEditInput}>수정</button>
+              </div>
+
+              <div style={{ display: showEdit ? "none" : "block" }}>
+                <input
+                  type="password"
+                  placeholder="비밀번호 입력"
+                  onChange={pwChangeHandler}
+                />
+                <input
+                  type="text"
+                  id="title"
+                  value={state.title}
+                  onChange={(e) => {
+                    const { id, value } = e.target;
+                    setState({ ...state, [id]: value });
+                  }}
+                  placeholder="제목을 입력해주세요"
+                />
+                <textarea
+                  id="content"
+                  value={state.content}
+                  onChange={(e) => {
+                    const { id, value } = e.target;
+                    setState({ ...state, [id]: value });
+                  }}
+                  placeholder="내용을 입력해주세요"
+                />
+                <button
+                  onClick={(e) => {
+                    onEditHandler(e);
+                  }}
+                >
+                  수정완료
+                </button>
+                <button onClick={toggleEditInput}>취소</button>
+              </div>
+
+              <div style={{ display: showDelete ? "none" : "block" }}>
+                <input
+                  type="password"
+                  onChange={(e) => setPWTest(e.target.value)}
+                  placeholder="비밀번호"
+                  value={pwTest}
+                />
+                <button onClick={() => onDeleteHandler(item.id)}>
+                  삭제완료
+                </button>
+                <button onClick={toggleDeleteInput}>취소</button>
+              </div>
+            </div>
+          );
+        })}
+    </>
+  );
+};
+
+export default BoardItemDetail;
--- src/pages/BoardDetail.jsx
+++ src/pages/BoardDetail.jsx
@@ -1,63 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
 import Layout from "../components/UI/Layout";
-import { __deleteBoards, __getBoards } from "../redux/modules/boardSlice";
-import { useNavigate } from "react-router-dom";
-import BoardUpdate from "../components/features/board/BoardUpdate";
+import BoardItemDetail from "../components/features/board/BoardItemDetail";
 
 const Detail = () => {
-  const dispatch = useDispatch();
-  const board = useSelector((state) => state.boardSlice.board);
-  const navigator = useNavigate();
-  const param = useParams().id;
-  //console.log(board);
-
-  // 비밀번호 일치 시 삭제
-  const a = board.filter((item) => item.id === param).map((item) => item)[0];
-  const b = a["pw"];
-  const [pwTest, setPWTest] = useState("");
-
-  const pwChangeHandler = (event) => {
-    setPWTest(event.target.value);
-  };
-
-  useEffect(() => {
-    dispatch(__getBoards());
-  }, [dispatch]);
-
-  // delete handler
-  const onDeleteHandler = (id) => {
-    if (pwTest === b) {
-      dispatch(__deleteBoards(id));
-      navigator("/");
-    } else {
-      alert("비밀번호가 일치하지 않습니다.");
-      return;
-    }
-  };
-
   return (
     <Layout>
-      {board
-        .filter((item) => item.id === param)
-        .map((item) => {
-          return (
-            <div key={item.id}>
-              <div>{item.title}</div>
-              <div>{item.content}</div>
-              <div>비밀번호 임시: {item.pw}</div>
-              <button onClick={() => onDeleteHandler(item.id)}>삭제</button>
-              <input
-                type="password"
-                placeholder="비밀번호 입력"
-                onChange={pwChangeHandler}
-              />
-            </div>
-          );
-        })}
-      <BoardUpdate />
-      <div>새로고침?</div>
+      <BoardItemDetail />
     </Layout>
   );
 };
