2c095e3e938ffc2d75dbaefe01e9d5f3f2e60b2e
seoyeon-jung <jungluchia1204@gmail.com>
2022-12-28 10:26:36 +0900

    Design: footer 수정

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -3,27 +3,16 @@ import styled from "styled-components";
 const Footer = () => {
   return (
     <StFooter>
-      <span>copyright 팔팔하정</span>
+      <span>
+        copyright ⓒ 팔팔하정
+        https://github.com/react-teamProject/react-teamproject
+      </span>
     </StFooter>
   );
 };
 
 export default Footer;
 
-// 지금 style은 임시 (나중에 css 수정 시 변경 예정)
-// footer 하단에 고정 완료 (추후 center로 위치 변경 필요)
 const StFooter = styled.footer`
-  /* font-size: 20px;
   color: #919191;
-  /* position: relative;
-  bottom: 0;
-  height: 70px; */
-  background-color: blue;
-  /* footer의 높이 */
-  /* position: absolute;
-  bottom: 0;
-  /* left: 0; */
-  /* position: relative;
-  top: 1000px;
-  transform: translateY(-100%);  */
 `;
